e26985d4b77a5d10fcd3fd42d4871513fd767f80
Hilal Ahmad <hilalahmedbutt@gmail.com>
2022-03-30 20:25:31 +0530

    add page param and limit,skip in post list,fetch single post

--- posts.js
+++ posts.js
@@ -1,25 +1,51 @@
 var postRoutes = require("express").Router()
+const { response } = require("express");
+const mongoose = require('mongoose');
 
+main().catch(err => console.log(err));
 
-postRoutes.get("/",(req,res)=>{
-    res.json("get all posts")
+async function main() {
+  await mongoose.connect('mongodb://localhost:27017/blog');
+}
+
+const postSchema = new mongoose.Schema({
+    title: String,
+    description: String
+});
+
+const Post = mongoose.model('Post', postSchema);
+
+postRoutes.get("/",(req,res) => {
+    var {page} = req.query
+    // res.json(page)
+    if(page=="" || page==null){
+        page = 1
+    }
+    Post.find({},(err,data)=>{
+        res.json(data)
+    }).skip((page-1)*7).limit(7)
 })
 
 postRoutes.get("/:postid",(req,res)=>{
-
-    res.json("get one post")
+    const {postid} = req.params
+    Post.findById(postid,(err,data)=>{
+        res.json(data)
+    })
+    
 })
 
-postRoutes.post("/",(req,res)=>{
-    res.json("create new post")
-})
+// postRoutes.post("/",(req,res)=>{
+//     res.json("create new post")
+// })
 
-postRoutes.put("/:postid",(req,res)=>{
-    res.json("update post")
-})
+// postRoutes.put("/:postid",(req,res)=>{
+//     //res.json("update post")
+//     const {id} = req.params
+//     Post.findByIdAndUpdate()
+// })
 
-postRoutes.delete("/:postid",(req,res)=>{
-    res.json("delete post")
-})
+// postRoutes.delete("/:postid",(req,res)=>{
+//     res.json("delete post")
+// })
 
 module.exports = postRoutes
